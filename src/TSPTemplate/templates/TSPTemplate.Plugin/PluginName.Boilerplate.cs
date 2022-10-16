a60fa0e66378b501304d79490a2be83ccccc2a62
Arthri <41360489@users.noreply.github.com>
2022-10-16 03:29:39 +0000

    Add project files

---
+++ src/TSPTemplate/templates/TSPTemplate.Plugin/PluginName.Boilerplate.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+namespace Company.Plugin;
+
+public partial class PluginName : TerrariaPlugin
+{
+    /// <inheritdoc />
+    public override string Name { get; } = typeof(PluginName).Assembly.GetCustomAttribute<AssemblyTitleAttribute>()?.Title ?? "Plugin";
+
+    /// <inheritdoc />
+    public override string Description { get; } = typeof(PluginName).Assembly.GetCustomAttribute<AssemblyDescriptionAttribute>()?.Description ?? "";
+
+    /// <inheritdoc />
+    public override Version Version { get; } = typeof(PluginName).Assembly.GetName()?.Version ?? new Version(0, 0);
+
+    /// <inheritdoc />
+    public override string Author { get; } = typeof(PluginName).Assembly.GetCustomAttribute<AssemblyCompanyAttribute>()?.Company ?? "Anonymous";
+
+    /// <summary>
+    /// The constructor for the plugin. Initialization should be done in <see cref="Initialize()" /> instead.
+    /// </summary>
+    public PluginName(Main game) : base(game)
+    {
+    }
+}
---
+++ src/TSPTemplate/templates/TSPTemplate.Plugin/PluginName.cs
@@ -0,0 +1,22 @@
+﻿namespace Company.Plugin;
+
+[ApiVersion(2, 1)]
+public partial class PluginName
+{
+    /// <inheritdoc />
+    public override void Initialize()
+    {
+    }
+
+    /// <inheritdoc />
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+        if (disposing)
+        {
+            // dispose managed resources
+        }
+
+        // dispose unmanaged resources
+    }
+}
